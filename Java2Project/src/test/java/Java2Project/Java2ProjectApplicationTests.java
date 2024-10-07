f8924080cc3e02fb351a7ac4eb183c1200fcf081
NCSUAL <ncsual@naver.com>
2024-10-07 22:44:14 +0900

    test: 테스트 코드 작성

--- Java2Project/src/test/java/Java2Project/Java2ProjectApplicationTests.java
+++ Java2Project/src/test/java/Java2Project/Java2ProjectApplicationTests.java
@@ -2,12 +2,16 @@ package Java2Project;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test") //test 프로파일을 활성화하여 테스트용 설정을 적용
+
 class Java2ProjectApplicationTests {
 
 	@Test
 	void contextLoads() {
+
 	}
 
 }
